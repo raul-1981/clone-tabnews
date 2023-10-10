ab29267a9073cf0f3e5be9ced1b52957e4c6a372
Raul VitorAraujo de Souza <68265981+raul-1981@users.noreply.github.com>
2023-10-10 22:50:06 +0000

    mudanca na frase de impacto do arquivo pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-	return <h1>Oi Amor, dedico minha primeira pagina na Web para voce!</h1>
+	return <h1>Oi, Nalva, Amor da minha Vida! Dedico minha primeira pagina na Web para voce!
+		Quando ver isso me de um beijo! Te Amo!!!
+	</h1>
 }
 
 export default Home;
